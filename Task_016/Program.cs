02abc09bed0a7dd01d58bc2762e86bc73891fc38
Petr-max <petrpuzaev@gmail.com>
2023-01-27 13:14:55 +0300

    Задача 16.

--- Task_016/Program.cs
+++ Task_016/Program.cs
@@ -8,3 +8,16 @@
 // 25,5-> Да 
 // 8,9-> Нет 
 
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool isSquare = IsSquare(number1, number2);
+
+Console.WriteLine(isSquare ? "Да" : "Нет");
+
+bool IsSquare(int num1, int num2)
+{
+  return num1 == num2*num2 || num2 == num1*num1;
+}
