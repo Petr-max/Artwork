45a6a0f9d6be777781e7055eff14eff25916b267
Petr-max <petrpuzaev@gmail.com>
2023-01-27 22:08:22 +0300

    Задача 13:

--- homework_13/Program.cs
+++ homework_13/Program.cs
@@ -7,7 +7,23 @@
 // 78-> третьей цифры нет
 // 32679-> 6
 
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
+int result = -1;
 
+if (n >= 100)
+{
+  while (n > 999)
+  {
+    n = n / 10;
+  }
+  result = n % 10;
+  Console.WriteLine(result);
+}
+if (n <= 99)
+{
+  Console.WriteLine($"Третьей цифры нет ");
+}
 
 
