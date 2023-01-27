a8410d60ddfe1a2451f4d0656612fcda2f51d2a7
Petr-max <petrpuzaev@gmail.com>
2023-01-27 12:17:46 +0300

    Задача 14.

--- Task_014/Program.cs
+++ Task_014/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 14.
+// Напишите программу, которая принимает
+// на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+
+// 14-> Нет
+// 46-> Нет 
+// 161-> Да
+
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+bool mult = Mult(number1);
+bool Mult(int num1)
+{
+  return num1 % 7 == 0 && num1 % 23 == 0;
+}
+Console.WriteLine(mult ? "Да" : "Нет" );
+
+
