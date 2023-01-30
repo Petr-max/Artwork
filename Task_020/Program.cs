df26824d4874b492bf1047a22829e70d50cc1fce
Petr-max <petrpuzaev@gmail.com>
2023-01-30 21:14:00 +0300

    Задача 22:

--- Task_020/Program.cs
+++ Task_020/Program.cs
@@ -17,12 +17,23 @@ Console.WriteLine("Введите координаты точки ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 
 double Distance(int xa, int ya, int xb, int yb)
+
 {
   int cat1 = xa - xb;
   int cat2 = ya - yb;
   double result = Math.Sqrt(cat1 * cat2 + cat2 * cat2);
   return result;
 }
+
 double distanse = Distanse(x1, y1, x2, y2);
 double dRound = Math.Round(distanse, 2, MidpointRounding.ToZero);
 Console.Write($"A ({x1} {y1}); B ({x2} {y2}) -> {dRound}");
+
+// double range = Distanse(a, a1, b, b1);
+// Console.WriteLine(Math.Round(range, 2, MidpointRounding.ToZero));
+
+// double Distance(int xa, int ya, int xb, int yb)
+// {
+//   double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+//   return result;
+// }
---
+++ Task_022/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 22:
+// Напишите программу, которая 
+// принимает на вход число (N)
+// и выдает таблицу квадратов 
+// чисел от 1 до (N).
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1, 4.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int num)
+{
+  int count = 1;
+  while (count <= num)
+  {
+    Console.WriteLine($"{count,5}, {count * count,5}");
+    count++;
+  }
+}
+SquareTable(number);
