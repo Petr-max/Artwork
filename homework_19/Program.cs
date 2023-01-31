7b213f860d49d10c8bb1eb324c044340173a9b1c
Petr-max <petrpuzaev@gmail.com>
2023-01-31 13:41:28 +0300

    Задача 19:

---
+++ homework_19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 19:
+
+// Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Enter five numbers: ");
+string numeral = Console.ReadLine()!;
+int len = numeral.Length;
+
+if (len == 5)
+{
+  if (numeral[0] == numeral[4] && numeral[1] == numeral[3])
+  {
+    Console.WriteLine($"{numeral} -> YES");
+  }
+  else
+  {
+    Console.WriteLine($"{numeral} -> NO");
+  }
+}
+else
+{
+  Console.WriteLine($"{numeral} -> Sorry, misentry:(( ");
+}
+
