44c99aa9aa55ce04e358b7946a8f3d4c8f086533
Petr-max <petrpuzaev@gmail.com>
2023-01-31 09:30:58 +0300

    Задача 9:

--- Task_009/Program.cs
+++ Task_009/Program.cs
@@ -9,27 +9,9 @@
 // 85-> 8
 
 int number = new Random().Next(10, 100);
-Console.WriteLine($"Случайное число из отрезка 10 -> 99 {number}");
-
-// int firstDigit = number / 10; // 7
-// int secondDigit = number % 10; // 8 
-
-// if (firstDigit > secondDigit)
-//   Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
-// else
-//   Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
-
-// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-// Console.WriteLine($"Наибольшая цифра числа -> {result}");
+Console.WriteLine($"Случайное число -> {number}");
 
 int maxDigit = MaxDigit(number);
-Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
-
-// int maxDigit1 = MaxDigit(23);
-// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit1}");
-
-// int maxDigit2 = MaxDigit(98);
-// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit2}");
 
 int MaxDigit(int num) // num = number 
 {
@@ -38,6 +20,4 @@ int MaxDigit(int num) // num = number
   int result = firstDigit > secondDigit ? firstDigit : secondDigit;
   return result;
 }
-
-// int maxDigit3 = MaxDigit(26);
-// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit3}");
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
