d530a80a3592104e69d4d8846c3a8a9ecb1b2df8
Petr-max <petrpuzaev@gmail.com>
2023-02-03 13:43:45 +0300

    Поищем максимум из 9 чисел.

---
+++ Lesson_02/Program.cs
@@ -0,0 +1,197 @@
+﻿// Поищем максимум из 9 чисел.
+
+
+// Next(1, 10);
+// WriteLine(“Привет”);
+
+// double f(double x)
+// {
+//   double result = x * x + 1;
+//   result result
+// }
+
+// if (b1 > max) max = b1;
+// if (c1 > max) max = c1;
+// if (a2 > max) max = a2;
+// if (b2 > max) max = b2;
+// if (c2 > max) max = c2;
+// if (a3 > max) max = a3;
+// if (b3 > max) max = b3;
+// if (c3 > max) max = c3;
+
+// Console.WriteLine(max);
+
+////////////////////////////////////////
+
+// int Max(int ard1, int ard2, int ard3)
+// {
+//   int result = arg1;
+//   if (arg2 > result) result = arg2;
+//   if (arg3 > result) result = arg3;
+//   return result;
+// }
+// if (b1 > max) max = b1;
+// if (c1 > max) max = c1;
+// if (a2 > max) max = a2;
+// if (b2 > max) max = b2;
+// if (c2 > max) max = c2;
+// if (a3 > max) max = a3;
+// if (b3 > max) max = b3;
+// if (c3 > max) max = c3;
+
+// int max1 = Max(a1, b1, c1);
+
+// int max1 = Max(a1, b1, c1);
+// int max1 = Max(a2, b2, c2);
+// int max1 = Max(a3, b3, c3);
+
+// int max1 = Max(max1, max2, max3);
+// Console.WriteLine(max);
+
+// int max = Max
+// (
+// Max(a1, b1, c1)
+// Max(a2, b2, c2)
+// Max(a3, b3, c3)
+// );
+
+// ////////////////////////
+
+// int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+// Console.WriteLine(array[0]);
+
+// array[0] = 12
+// Console.WriteLine(array[0]);
+
+// Console.WriteLine(array[4]);
+
+// int max = Max(
+//  Max(array[0], array[1], array[2],
+// Max(array[3], array[4], array[5],
+// Max(array[6], array[7], array[8]
+// )
+
+// int max = Max(
+//  Max(array[0], array[1], array[2],
+// Max(array[3], array[4], array[5],
+// 14
+// Max(array[6], array[7], array[8]
+// )
+// Console.WriteLine(max);
+
+// int result = Max(
+//  Max(array[0], array[1], array[2],
+// Max(array[3], array[4], array[5],
+// Max(array[6], array[7], array[8]
+// )
+// Console.WriteLine(result);
+
+// //////////////////////
+
+// int[] array = {1,12,31,4,15,16,17,18};
+
+// int n = array.Length;
+
+// int find = 4;
+
+// Int index = 0;
+
+// while (index < n)
+// 16
+// {
+// index = index + 1;
+// }
+// while (index < n)
+// {
+// index + +;
+// }
+// while (index < n)
+// {
+// if(array[index] == find)
+// index + +;
+// }
+// while (index < n)
+// {
+// if(array[index] == find)
+// {
+// Console.WriteLine(index);
+// }
+// index + +;
+// }
+
+// //////////////////////
+
+// int[] array = {1,12,31,4,18,15,16,17,18};
+
+// if(array[index] == find)
+// {
+// Console.WriteLine(index);
+// break;
+// }
+
+// //////////////////////
+
+// int[] array = new int[10];
+
+// void FillArray(int[] collection)
+// {
+// int length = collection.Length;
+// int index = 0;
+// while (index < length)
+// {
+//  //index = index + 1;
+//  Index++;
+// }
+// }
+
+// //////////////////////
+
+// void FillArray(int[] collection)
+// {
+// int length = collection.Length;
+// int index = 0;
+// while (index < length)
+// 19
+// {
+//  collection[index] = new Random().Next(1, 10);
+// //index = index + 1;
+//  Index++;
+// }
+// }
+
+// ///////////////////
+
+// void FillArray(int[] collection)
+// void PrintArray(int[] col)
+
+// int count = col.Length;
+
+// int position = 0;
+
+// while (position < count);
+
+// Console.WriteLine(col[position]);
+// position++;
+
+// FillArray(array);
+// PrintArray(array);
+
+////////////////////////
+
+// int IndexOf(int[] collection, int find)
+// {
+// int index = collection.Length;
+// int count = 0;
+// int position = 0;
+// while (index < count)
+// {
+//  If(collection[index] == find)
+//  {
+//  position = index;
+//  }
+// index++;
+// }
+// Return position;
+// }
+// int pos = IndexOf(array, 4);
+// Console.WriteLine(pos);
