4e0698962d77bef16d246ce84dc6fcf61a98a2ac
Petr-max <petrpuzaev@gmail.com>
2023-02-04 16:31:33 +0300

    Задача 27:

---
+++ homework_27/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int number = ReadInt("Введите число: ");
+
+int vst = NumberVst(number);
+SumNumbers(number, vst);
+
+int NumberVst(int a)
+{
+  int index = 0;
+  while (a > 0)
+  {
+    a /= 10;
+    index++;
+  }
+  return index;
+}
+void SumNumbers(int n, int vst)
+{
+  int sum = 0;
+  for (int i = 1; i <= vst; i++)
+  {
+    sum += n % 10;
+    n /= 10;
+  }
+  Console.WriteLine($"сумма цифр {sum}");
+}
+int ReadInt(string message)
+{
+  Console.Write(message);
+  return Convert.ToInt32(Console.ReadLine());
+}
