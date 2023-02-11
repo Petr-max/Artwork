d4ea456a7ca264ca7409ba41ddbf11da4528d21d
Petr-max <petrpuzaev@gmail.com>
2023-02-11 14:52:14 +0300

    Задача 42v2

--- Task_042v2/Program.cs
+++ Task_042v2/Program.cs
@@ -9,19 +9,19 @@
 
 string Binary(int num)
 {
-    string res = string.Empty;
-    while(num > 0)
-    {
+  string res = string.Empty;
+  while (num > 0)
+  {
     res = num % 2 + res;
     num /= 2;
-    }
-    return res;
+  }
+  return res;
 }
 Console.Write("Введите число: ");
 int num;
 if (int.TryParse(Console.ReadLine(), out num))
 {
-    string res = Binary(num);
-    Console.WriteLine(res);
+  string res = Binary(num);
+  Console.WriteLine(res);
 }
 else Console.WriteLine("Введено некорректное значение.");
