b56abc39add4fbf8108b9117ee9d364353de2224
Petr-max <petrpuzaev@gmail.com>
2023-02-12 15:40:46 +0300

    Заметка

--- Task_049/Program.cs
+++ Task_049/Program.cs
@@ -8,13 +8,14 @@
 5 9 2 3         5 9 2  3
 8 4 2 4         64 4 4 4 */
 
-int[,] CreateMatrixRnd(int rows, int columns, int min, int max)
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-  Random rnd = new Random();
   int[,] matrix = new int[rows, columns];
-  for (int i = 0; i < rows; i++)
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
   {
-    for (int j = 0; j < columns; j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
       matrix[i, j] = rnd.Next(min, max + 1);
     }
@@ -28,25 +29,24 @@ void PrintMatrix(int[,] matrix)
     Console.Write("[");
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-      if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
-      else Console.Write($"{matrix[i, j],3} ");
+      if (j < matrix.GetLength(1) - 1)
+        Console.Write($"{matrix[i, j],4} | ");
+      else Console.Write($"{matrix[i, j],4} ");
     }
     Console.WriteLine("]");
   }
 }
-void SquareOfElements(int[,] matrix)
+void SquareAddNambers(int[,] matrix)
 {
-  for (int i = 0; i < matrix.GetLength(0); i += 2)
-  {
-    for (int j = 0; j < matrix.GetLength(1); j += 2)
-
-      matrix[i, j] *= matrix[i, j];
+  for (int i = 0; i < matrix.GetLength(0); i += 2) // проход сразу по четным числам строк += 2
+  {                                                 // провека на четность не потребуется
+    for (int j = 0; j < matrix.GetLength(1); j += 2) // проход сразу по четным числам столбцов += 2
+      matrix[i, j] *= matrix[i, j]; // возводим в квадрат перемножаем 
   }
 }
-int[,] array2D = CreateMatrixRnd(3, 4, 1, 10);
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
 PrintMatrix(array2D);
-//int[,] squareOfElements = SquareOfElements(array2D);
-Console.WriteLine($"{'|',10}");
-Console.WriteLine($"{'V',10}");
-SquareOfElements(array2D);
+Console.WriteLine($"{'|',14}");
+Console.WriteLine($"{'V',14}");
+SquareAddNambers(array2D);
 PrintMatrix(array2D);
