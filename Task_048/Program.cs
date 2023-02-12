02d16463bd563970a96399d89a3fa07db6e49727
Petr-max <petrpuzaev@gmail.com>
2023-02-12 13:53:47 +0300

    Задача 48: исправил

--- Task_048/Program.cs
+++ Task_048/Program.cs
@@ -9,49 +9,33 @@
 // 1 2 3 4
 // 2 3 4 5
 
-int[,] dimensionalMass = CreateMatrixRndInt(4, 4, 0, 10);
-PrintMatrix(dimensionalMass);
-Console.WriteLine();
-ReplaceMatrix(dimensionalMass);
-PrintMatrix(dimensionalMass);
+int[,] CreateMatrixArray(int rows, int columns)
+{
+  int[,] arr = new int[rows, columns];
 
-void ReplaceMatrix(int[,] inMatrix)
-{
-  for (int i = 0; i < inMatrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < inMatrix.GetLength(1); j++)
-    {
-      if (i % 2 == 0 && j % 2 == 0)
-      {
-        inMatrix[i, j] *= inMatrix[i, j];
-      }
-    }
-  }
-}
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-{
-  int[,] matrix = new int[rows, columns]; // 0, 1
-  Random rnd = new Random();
 
-  for (int i = 0; i < matrix.GetLength(0); i++)
+  for (int i = 0; i < rows; i++)
   {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int j = 0; j < columns; j++)
     {
-      matrix[i, j] = rnd.Next(min, max + 1);
+      arr[i, j] = i + j;
     }
   }
-  return matrix;
+  return arr;
 }
-void PrintMatrix(int[,] matrix)
+void PrintMatrixArray(int[,] arr)
 {
-  for (int i = 0; i < matrix.GetLength(0); i++)
+  for (int i = 0; i < arr.GetLength(0); i++)
   {
-    Console.Write("|");
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    Console.Write("[");
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-      if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
-      else Console.Write($"{matrix[i, j],4} ");
+      if (j < arr.GetLength(1) - 1)
+        Console.Write($"{arr[i, j],3} |");
+      else Console.Write($"{arr[i, j],3}");
     }
-    Console.WriteLine("|");
+    Console.WriteLine("]");
   }
 }
+int[,] array2D = CreateMatrixArray(5, 6);
+PrintMatrixArray(array2D);
